a530b8a3c1042261b6cb430d628b9338a5036659
Kyaw Zay Ya <kyawzaya3578@gmail.com>
2025-11-25 06:58:46 +0700

    make changes in TodoFilters.js

--- src/components/TodoFilters.js
+++ src/components/TodoFilters.js
@@ -1,37 +1,27 @@
 import React, { useEffect } from "react";
 
 export default function TodoFilters({ filterBy }) {
-  let [filter, setFilter] = React.useState("All");
+  const [filter, setFilter] = React.useState("All");
+
   useEffect(() => {
     filterBy(filter);
-  }, [filter]);
+  }, [filter, filterBy]);
+
+  const filters = ["All", "Active", "Completed"];
 
   return (
     <div>
-      <button
-        className={`button filter-button ${
-          filter === "All" ? "filter-button-active" : ""
-        }`}
-        onClick={() => setFilter("All")}
-      >
-        All
-      </button>
-      <button
-        className={`button filter-button ${
-          filter === "Active" ? "filter-button-active" : ""
-        }`}
-        onClick={() => setFilter("Active")}
-      >
-        Active
-      </button>
-      <button
-        className={`button filter-button ${
-          filter === "Completed" ? "filter-button-active" : ""
-        }`}
-        onClick={() => setFilter("Completed")}
-      >
-        Completed
-      </button>
+      {filters.map((filterType) => (
+        <button
+          key={filterType}
+          className={`button filter-button ${
+            filter === filterType ? "filter-button-active" : ""
+          }`}
+          onClick={() => setFilter(filterType)}
+        >
+          {filterType}
+        </button>
+      ))}
     </div>
   );
 }
